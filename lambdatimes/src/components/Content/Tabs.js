64a37f05f54f9fb411e8c161337a4d95de650abb
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-22 18:48:45 -0700

    Added key to tabs being rendered

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,14 +8,14 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
-            props.tabs.map(tab => <Tab selectTabHandler={props.selectTabHandler} selectTab={props.selectTab} tab={tab}/>)}
+            props.tabs.map((tab, index) => <Tab key={index} selectTabHandler={props.selectTabHandler} selectTab={props.selectTab} tab={tab}/>)}
       </div>
     </div>
   );
 };
 
  Tabs.propTypes = {
-  tabs : PropTypes.arrayOf(PropTypes.object),
+  tabs : PropTypes.arrayOf(PropTypes.string),
   selectTabHandler : PropTypes.func,
   selectTab : PropTypes.func
 }
